20297d815a70363d3fe7da3f359fc14a25b832ba
Yulia <122625932+Uya13@users.noreply.github.com>
2026-05-22 22:39:59 +0300

    Update message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Continuous Deployment!'
+    return 'Hello, Continuous Deployment! Updated!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
